478185a2657587f4a98350fb884d948df447c657
EngHassan8 <hassannaana99@gmail.com>
2025-08-29 20:01:01 +0300

    last Updat  backend

--- src/pages/election/Elections.jsx
+++ src/pages/election/Elections.jsx
@@ -32,7 +32,7 @@ function Elections() {
   const HandleNewElection = (e) => {
     e.preventDefault();
 
-    axios.post("https://back-1-374m.onrender.com/new/elction", {
+    axios.post("https://back-1-374m.onrender.com/new/election", {
       Name,
       Postion,
       StartDate,
